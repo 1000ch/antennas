6e73581483796176da17fd5325e659ee437091a8
1000ch <shogo.sensui@gmail.com>
2015-03-09 01:23:27 +0900

    routing

--- public/js/main.js
+++ public/js/main.js
@@ -1,11 +1,31 @@
 antennas.collection.itemList = new antennas.collection.ItemList();
+
 antennas.view.itemListView = new antennas.view.ItemListView({
   collection: antennas.collection.itemList
 });
+
 antennas.view.paginateView = new antennas.view.PaginateView({
   collection: antennas.collection.itemList
 });
 
+antennas.Router = Backbone.Router.extend({
+  routes: {
+    '': 'index',
+    'login': 'login'
+  },
+  initialze: function () {
+  },
+  onRoute: function () {},
+  index: function () {
+    antennas.collection.itemList.fetch();
+  },
+  login: function () {
+  }
+});
+
 $(function () {
-  antennas.collection.itemList.fetch();
+  antennas.router = new antennas.Router();
+  Backbone.history.start({
+    pushState: true
+  });
 });
