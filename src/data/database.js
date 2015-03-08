c4017bde0ad878dd86a1bdd6c681bbdbbbb8a421
1000ch <shogo.sensui@gmail.com>
2015-03-08 15:09:24 +0900

    fix dbname

--- src/data/database.js
+++ src/data/database.js
@@ -2,13 +2,12 @@
 
 var monk = require('monk');
 
-const dbname   = 'rss';
-const mongoUrl = process.env.MONGOLAB_URI ||
-                 `mongodb://127.0.0.1:27017/${dbname}`;
+const dbname   = 'antennas';
+const hostname = process.env.MONGOLAB_URI || `mongodb://127.0.0.1:27017/${dbname}`;
 
 class Database {
   constructor() {
-    this.db = monk(mongoUrl);
+    this.db = monk(hostname);
   }
   collection(name) {
     return this.db.get(name);
